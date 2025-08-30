5e555575c21986f150f3b76d6923d4f71fb01521
FelipeNeves <felipewordmundo123@gmail.com>
2025-08-30 11:44:27 -0300

    Primeiro commit,criado as pasta do cleancode e a coneccao do DB

---
+++ Event_Code/src/main/java/dev/feneves/Event_Code/EventCodeApplication.java
@@ -0,0 +1,13 @@
+package dev.feneves.Event_Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCodeApplication.class, args);
+	}
+
+}
---
+++ Event_Code/src/test/java/dev/feneves/Event_Code/EventCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.feneves.Event_Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
